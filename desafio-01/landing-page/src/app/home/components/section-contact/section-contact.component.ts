5dd3340c6f0db42d42625927cf06f7b2fa9e6817
Prates <guipratesdsantos@gmail.com>
2024-01-19 17:22:40 -0300

    check-speech revisao

--- desafio-01/landing-page/src/app/home/components/section-contact/section-contact.component.ts
+++ desafio-01/landing-page/src/app/home/components/section-contact/section-contact.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './section-contact.component.html',
   styleUrls: ['./section-contact.component.css','./section-contact.responsive.component.css']
 })
-export class SectionContactComponent {
 
+export class SectionContactComponent {
+  //Checagem do estado do checkbox para envio do formulário
+  checkboxState: boolean = false;
+  botaoDesabilitado: boolean = true;
+
+  onCheckboxChange() {
+    this.botaoDesabilitado = !this.checkboxState;
+  }
+
+  //Formatação do input(Numero de telefone)
+  formattedValue: string = '';
+
+  getNumber(getN: string) {
+    const numericValue: string = getN.replace(/\D/g, '');
+    this.formattedValue = this.formatarTelefone(numericValue);
+    
+  }
+
+  formatarTelefone(value: string): string {
+    if (value.length <= 2) {
+      return `(${value}`;
+    } else if (value.length <= 6) {
+      return `(${value.slice(0, 2)}) ${value.slice(2)}`;
+    } else {
+      return `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7, 10)}`;
+    }
+  }
+  
 }
